92dae90b2c282f7d5cb81d2cc620220bf7dc12a5
christopherahk <christopher.ahk@gmail.com>
2025-01-06 23:00:44 +0100

    Added classes ReplayMemory and DQN
    Made two files ReplayMemory.py and DQN.py containing classes with code taken from the pytorch documentation (DQN tutorial).

---
+++ ReplayMemory.py
@@ -0,0 +1,22 @@
+from collections import deque, namedtuple
+import random
+
+
+class ReplayMemory(object):
+    Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
+
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    
+    def push(self, *args):
+        self.memory.append(self.Transition(*args))
+
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    
+    
+    def __len__(self):
+        return len(self.memory)
