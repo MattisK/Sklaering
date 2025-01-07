8ea043b83a5eaaba210fcf2ec610bcf89b85f0a1
christopherahk <christopher.ahk@gmail.com>
2025-01-07 17:29:34 +0100

    Updated DQN.py and functions.py
    DQN is undone, but is being switched to a convolutional neural network. Functions now has documentation.

--- DQN.py
+++ DQN.py
@@ -1,17 +1,15 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class DQN(nn.Module):
-    def __init__(self, n_observations, n_actions): # on init we define the layers of the network
+    def __init__(self):
         super(DQN, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 128) # currently we have 3 layers maximum number of moves # 4672
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, n_actions)
+        self.conv1 = nn.Conv2d(12, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(128 * 8 * 8, 1024)
+        self.fc2 = nn.Linear(1024, 64 * 64)
 
     
-    def forward(self, x): # take input x and pass it through the layers
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        return self.layer3(x)
+    def forward(self, x):
+        pass
     
--- functions.py
+++ functions.py
@@ -3,19 +3,28 @@ import numpy as np
 import chess
 
 
-def board_to_tensor(board: chess.Board):
+def board_to_tensor(board: chess.Board) -> torch.Tensor:
+    """
+    Takes the board state and converts it to a one-hot tensor.
+    """
+    # An 8x8 board with a third dimension of size 12 to separate the pieces into different planes.
     np_board = np.zeros((12, 8, 8))
+
+    # Gives a value to each of the types of pieces.
     piece_map = {"p": 0, "n": 1, "b": 2, "r": 3, "q": 4, "k": 5,
                  "P": 6, "N": 7, "B": 8, "R": 9, "Q": 10, "K": 11}
     
+    # Inserts a 1 in each of the pieces' planes where they are located.
     for square, piece in board.piece_map().items():
         plane = piece_map[str(piece)]
         row, col = divmod(square, 8)
         np_board[plane, row, col] = 1
     
+    # Returns a tensor of the one-hot
     return torch.tensor(np_board, dtype=torch.float32)
 
 
-board = chess.Board()
+if __name__ == "__main__":
+    board = chess.Board()
 
-print(board_to_tensor(board))
+    print(board_to_tensor(board))
