d6c9c75699e085f9813061739604b1bebe98e9fd
christopherahk <christopher.ahk@gmail.com>
2025-01-22 13:16:04 +0100

    Bar chart for elo wins and draws

--- test_model_old.py
+++ test_model_old.py
@@ -4,6 +4,7 @@ from functions import get_best_move
 from stockfish import Stockfish
 import torch
 import random
+import numpy as np
 
 
 def play_game(model: ChessCNN, board: chess.Board, stockfish: Stockfish) -> None:
@@ -30,36 +31,48 @@ def play_game(model: ChessCNN, board: chess.Board, stockfish: Stockfish) -> None
 if __name__ == "__main__":
     # Load the trained model.
     model = ChessCNN()
-    model.load_state_dict(torch.load("chess_model_early_stopping.pth"))
+    model.load_state_dict(torch.load("chess_model_early_stopping.pth", map_location=torch.device("cpu")))
 
     # Set the model to evaluation mode.
     model.eval()
 
     # Initialize stockfish and set the skill level.
-    stockfish_path = "C:/Users/chris/Desktop/Stockfish/stockfish/stockfish-windows-x86-64-avx2"
-    #stockfish_path = "C:/Users/chris/OneDrive/Desktop/stockfish/stockfish/stockfish-windows-x86-64-avx2"
+    #stockfish_path = "C:/Users/chris/Desktop/Stockfish/stockfish/stockfish-windows-x86-64-avx2"
+    stockfish_path = "C:/Users/chris/OneDrive/Desktop/stockfish/stockfish/stockfish-windows-x86-64-avx2"
     stockfish = Stockfish(stockfish_path, depth=1)
-    stockfish.set_skill_level(0)
+    stockfish.set_elo_rating(1400)
 
     # Board object.
     board = chess.Board()
     
     # Keeps track of the results.
-    results = {"White": 0, "Black": 0, "Draw": 0}
-    num_games = 100
-    for i in range(num_games):
-        # Runs loop for each game and appends the result to the results dictionary,
-        # then resets the board when the game is over and prints the result when all games are over.
-        play_game(model, board, stockfish)
-        print("Game:", i + 1)
-        result = board.result()
-        if result == "1-0":
-            results["White"] += 1
-        elif result == "0-1":
-            results["Black"] += 1
-        elif result == "1/2-1/2":
-            results["Draw"] += 1
+    wins = []
+    draws = []
+    for j in range(31):
+        results = {"White": 0, "Black": 0, "Draw": 0}
+        num_games = 100
+        stockfish.set_elo_rating(j * 100)
+        for i in range(num_games):
+            # Runs loop for each game and appends the result to the results dictionary,
+            # then resets the board when the game is over and prints the result when all games are over.
+            play_game(model, board, stockfish)
+            print("Game:", i + 1)
+            result = board.result()
+            if result == "1-0":
+                results["White"] += 1
+            elif result == "0-1":
+                results["Black"] += 1
+            elif result == "1/2-1/2":
+                results["Draw"] += 1
 
-        board.reset()
+            board.reset()
 
-print(results)
+        wins.append(results["White"])
+        draws.append(results["Draw"])
+        print(f"Elo {j * 100} done.")
+        print(results)
+    np_wins = np.array(wins)
+    np_draws = np.array(draws)
+
+    np.save("wins.npy", np_wins, allow_pickle=True)
+    np.save("draws.npy", np_draws, allow_pickle=True)
